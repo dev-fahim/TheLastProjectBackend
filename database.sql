a47368222695549ea62855d8facd5570f2bc80d7
dev-fahim <fahim6668@gmail.com>
2019-10-04 20:45:08 +0000

    changed sql file error

--- database.sql
+++ database.sql
@@ -2,8 +2,8 @@ CREATE DATABASE bokadjangodatabase;
 
 CREATE USER bokadjangouser WITH PASSWORD 'Boka2019Database';
 
-ALTER ROLE myprojectuser SET client_encoding TO 'utf8';
-ALTER ROLE myprojectuser SET default_transaction_isolation TO 'read committed';
-ALTER ROLE myprojectuser SET timezone TO 'UTC';
+ALTER ROLE bokadjangouser SET client_encoding TO 'utf8';
+ALTER ROLE bokadjangouser SET default_transaction_isolation TO 'read committed';
+ALTER ROLE bokadjangouser SET timezone TO 'UTC';
 
 GRANT ALL PRIVILEGES ON DATABASE bokadjangodatabase TO bokadjangouser;
